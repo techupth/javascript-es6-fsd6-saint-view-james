fada2e32691f0757892735889ee82fcf785f4988
James <apisitamornktn@gmail.com>
2024-05-24 12:54:15 +0700

    feat:JavaScript ES6 Assignment

--- ex-2.js
+++ ex-2.js
@@ -13,5 +13,7 @@ let studentScores = {
 };
 
 // Start coding here
+let newStudentProfile = { ...studentProfile };
+newStudentProfile.scores = { ...studentProfile.scores, ...studentScores };
 
 console.log(newStudentProfile);
